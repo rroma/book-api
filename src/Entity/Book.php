f1326a2615b9e3939595fdea7d0b92edb9050ee4
Roman Romanov <rroma.dev@gmail.com>
2020-01-21 15:43:51 +0300

    Book list filters + open get/list endpoints

--- src/Entity/Book.php
+++ src/Entity/Book.php
@@ -3,10 +3,29 @@
 namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\RangeFilter;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ApiResource()
+ * @ApiResource(
+ *     itemOperations={
+ *         "get",
+ *         "put"={"access_control"="is_granted('ROLE_USER') and object.author = user"},
+ *         "delete"={"access_control"="is_granted('ROLE_USER') and object.author = user"},
+ *         "patch"={"access_control"="is_granted('ROLE_USER') and object.author = user"}
+ *     },
+ *     collectionOperations={
+ *         "get",
+ *         "post"={"access_control"="is_granted('ROLE_USER')"}
+ *     }
+ * )
+ * @ApiFilter(SearchFilter::class, properties={
+ *     "title": "partial",
+ *     "description": "partial",
+ *     "author.authorPseudonym": "partial"
+ * })
  * @ORM\Entity(repositoryClass="App\Repository\BookRepository")
  */
 class Book
